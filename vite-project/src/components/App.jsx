aba5d9b7235987017c71ce9ebc30fe82ac7fd919
aarondavidreal-lab <aarondavid.real@gmail.com>
2026-02-05 11:34:18 +0100

    get form data

--- vite-project/src/components/App.jsx
+++ vite-project/src/components/App.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react"
+
 export default function App() {
+
+    const [formData, setFormData] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: ""
+    })
+
+    function handleChange(e) {
+        e.preventDefault()
+
+        const {name, value} = e.target
+        setFormData(prevData => ({...prevData, [name] : value}))
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        console.log(formData)
+    }
+
 return(
     <main>
-
         <div className="intro-div">
                 <h1 className="intro">Learn to code by watching others</h1>
                 <p className="intro">See how experienced developer solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable</p>
@@ -13,11 +35,14 @@ return(
                 <p>Try it free 7 days <span>then $20/mo. thereafter</span></p>
             </div>
 
-            <form>
-                <input placeholder="First Name" required type="text"/>
-                <input placeholder="Last Name" required type="text"/>
-                <input placeholder="Email Address" required type="email"/>
-                <input placeholder="Password" required type="password"/>
+            <form onSubmit={handleSubmit}>
+                <input placeholder="First Name" required 
+                name="firstName" onChange={handleChange} value={formData.firstName} type="text"/>
+                <input placeholder="Last Name" required onChange={handleChange} 
+                name="lastName" value={formData.lastName} type="text"/>
+                <input placeholder="Email Address" required onChange={handleChange} value={formData.email}
+                name="email" type="email"/>
+                <input placeholder="Password" required onChange={handleChange} name="password" value={formData.password} type="password"/>
                 <button type="submit">CLAIM YOUR FREE TRIAL</button>
 
                 <p>By clicking the button you are agreeing to our <span>Terms and Services</span></p>
